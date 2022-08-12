8d0915591be4ad83b003e99bed78706dd0a09f45
Robert Edmonds <bobby.edmonds89@gmail.com>
2022-08-12 16:08:09 -0400

    commit tree fec4f00b0c7a5bef86623025270d81fc0504a5ff

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length:{maximum: 250}
+    validates :category, inclusion: {in: (["Fiction", "Non-Fiction"])}
+    validate :title_validates_clickbait
+
+    OPTIONS = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i]
+
+    def title_validates_clickbait
+        if OPTIONS.none? { |option| option.match(title)} 
+            errors.add(:title, "Need to add more for clickbait")
+        end
+    end
 end
